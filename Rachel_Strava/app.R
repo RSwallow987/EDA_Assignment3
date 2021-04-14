8419c48b8de8ee84ab102889697d88f1660e60c1
RSwallow987 <rswallow987@gmail.com>
2021-04-14 16:03:03 +0200

    Working on timing of activity

--- Rachel_Strava/app.R
+++ Rachel_Strava/app.R
@@ -11,39 +11,88 @@
 #Load Librarys
 library(shiny)
 library(leaflet)
+library(raster)
 library(spData)
 library(dplyr)
 library(tidyverse)
 library(leaftime)
+library(lubridate)
 
 #Load data
 my_data <- readRDS("~/2021/EDA/EDA workspace/EDA_Assignment3/Rachel_Strava/data/my_data.rds")#Is this a probelm 
-x<-c(1,2,3,4,5)
+#x<-c(1,2,3,4,5)
+# handling the data conversions 
+my_data<- lapply(my_data, function(x) {
+  x$date <- ymd(x$date)
+  x$time<-hms(x$time)
+  x #why this ?
+})
 
 # Define UI for application that draws a histogram
 ui <- fluidPage(
   theme = shinythemes::shinytheme("united"),
   titlePanel("My Strava App"),
-  selectInput("run", label = "Run", choices = x),
-  verbatimTextOutput("summary"),
-  tableOutput("table")
+  #PAGE 1 
+  sidebarPanel(
+    dateInput('date',
+              label = 'Run Date: ',
+              value = "2021-03-01",
+              min="2019-02-27",#Minimum date of run (002.csv)
+              max="2021-03-01" #Maximum date of run (176.csv)
+    )
+   # PAGE 2
+    # dateRangeInput('dateRange',
+    #                label = 'Date Range: ',
+    #                start = "2021-02-01", end = "2021-03-01",#start on the last month of running
+    #                min="2019-02-27",#Minimum date of run (002.csv)
+    #                max="2021-03-01" #Maximum date of run (176.csv)
+    # )
+    ),
+  mainPanel(
+    leafletOutput("map"),
+     # tableOutput("table"),
+    textOutput("text")
+    )
 )
 
 # Define server logic required to draw a histogram
 server <- function(input, output, session) {
-  
   dataset <- reactive({
-    my_data[[as.numeric(input$run)]]
+    #r<-lapply(my_data, function(x) filter(x, date>=input$dateRange[1]&& date<=input$dateRange[2]))
+    r<-lapply(my_data, function(x) filter(x, date==input$date))
+    #For loop
+    ind<-c()
+    j<-1
+    for(i in 1:length(my_data)){
+       if (length(r[[i]]$date)>2){
+
+         ind[j]<-i
+         j<-j+1
+
+       }
+    }
+#What would I do here to separate the data frames given back ? would I use a lapply ? 
+     as.data.frame(my_data[ind])#gives back a list of non-empty dataframes
+   })
+  
+  geodata<-reactive({
+    st_as_sf(dataset(),coords = c("lat","lng","elevation"),crs = 4326)
   })
   
-  output$summary <- renderPrint({
-    # Use a reactive expression by calling it like a function
-    summary(dataset())
+  # output$table <- renderTable({
+  #   geodata()
+  #   })
+  
+  output$text <- renderText({
+    str(geodata())
+    hms(geodata()$time[length(geodata()$time)]-geodata()$time[1])
+
   })
   
-  output$table <- renderTable({
-    dataset()
-  })
+ output$map <- renderLeaflet({
+   leaflet() %>% addTiles() %>%addPolylines(lng=dataset()$lng, lat=dataset()$lat,col="#FC4C1A",popup="Running Route")#says its not subsetable
+ })#Plotted in Strava colors 
+   
 }
 # Run the application 
 shinyApp(ui = ui, server = server)
